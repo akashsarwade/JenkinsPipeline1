e559f2e47f3e4f3b31e3fa41eed050de169cc4db
Akash Sarwade <akshsarwade@gmail.com>
2022-09-23 15:50:11 +0530

    third commit

--- src/main/java/com/jenkinspipeline/CicdPipeline1Application.java
+++ src/main/java/com/jenkinspipeline/CicdPipeline1Application.java
@@ -5,7 +5,6 @@ import javax.annotation.PostConstruct;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.qos.logback.classic.Logger;
